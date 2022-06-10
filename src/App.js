98ea23b079db544e184d76ceab9666ff291f5fbf
karamalqinneh <karamalqinneh@live.com>
2022-06-10 23:44:20 +0300

    added the footer

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import "./App.css";
 import LoginProvider from "./context/auth/login";
 import ThemeProvider from "./context/theme/theme";
 import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <ThemeProvider>
       <LoginProvider>
         <Header />
+        <Footer />
       </LoginProvider>
     </ThemeProvider>
   );
